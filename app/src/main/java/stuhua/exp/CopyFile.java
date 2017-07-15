164ff573b1b766ab849690581a152b9e40e9d51b
stuhua <850360626@qq.com>
2017-07-15 17:13:27 +0800

    updata copy

--- app/src/main/java/stuhua/exp/CopyFile.java
+++ app/src/main/java/stuhua/exp/CopyFile.java
@@ -12,18 +12,55 @@ import java.util.logging.SimpleFormatter;
 
 public class CopyFile {
 	public static void main(String[] args) {
+		URI u = URI.create("file:///f:/1");
+		Path p = Paths.get(u);
+		try {
+			System.out.println("size=" + getDirSize(p));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	static long size = 0;
+
+	public static long getDirSize(Path sourcePath) throws IOException {
+
+		Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+
+			@Override
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				size += attrs.size();
+				System.out.println("file:"+file.getFileName()+"...size:"+attrs.size());
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFileFailed(Path file, IOException exc)
+					throws IOException {
+				System.out.println("visitFileFailed---:" + file.getFileName());
+				return FileVisitResult.TERMINATE;
+			}
+
+		});
+		return size;
+	}
+
+	private void method() {
+
 		try {
 			URI u = URI.create("file:///f:/1");
 			Path p = Paths.get(u);
 			Files.walkFileTree(p, new SimpleFileVisitor<Path>() {
 
-				private String pathName="1";
-				private String fileTarget="f:/2";
+				private String pathName = "1";
+				private String fileTarget = "f:/2";
 
 				@Override
 				public FileVisitResult preVisitDirectory(Path dir,
 						BasicFileAttributes attrs) throws IOException {
-					
+
 					String dirAbsPath = dir.toAbsolutePath().toString();
 					String subStringOfSource = dirAbsPath.substring(dirAbsPath
 							.indexOf(pathName) + pathName.length());
@@ -62,5 +99,6 @@ public class CopyFile {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 }
